b67d128d1fb59486a5daba7a5b98dc3845f3ff38
Christopher Shi <cshi@atlassian.com>
2021-08-21 13:16:48 +1000

    add basic corner

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
-
 import { LocationRow } from './LocationRow';
+import { Corner } from './Corner';
 
 import './css/board.css';
 
@@ -14,7 +14,10 @@ export const Board = () => {
 
     // 3 rows. Top row and bottom row contain two corners each. The middle row contains the entire vertical column of the left/right sides
     for (var i = 0; i < ROWS; i++) {
-        outerRows.push(<Row className="outer-row" sm={3}><LocationRow className="location-row" key={i} index={i}></LocationRow></Row>);
+        outerRows.push(<Row className="outer-row" sm={3}>
+            <Corner></Corner>
+            <LocationRow className="location-row" key={i} index={i}></LocationRow>
+            </Row>);
     }
 
     const board = (
